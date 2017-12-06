1d835d3108e5349f05a496ddc0f9adf9d7ef6ded
unknown <git@y-richter.de>
2017-12-06 19:15:10 +0100

    Changed example code

--- test_dps.py
+++ test_dps.py
@@ -1,20 +1,35 @@
 import pydps,time
 
 # dps Test Example
+dps = pydps.dps_psu('COM11', 1) # port name, slave address (in decimal)
+
+#make a csv like measurement
+def measureSteps(start,stop,step):
+
+    with open('results.csv', 'a') as csv: # open csv file and append
+        for v in range(int(start*1000),int((step+stop)*1000),int(step*1000)):
+            dps.setVoltage(v*0.001)
+            time.sleep(0.5) #wait for stable voltage
+            dat=dps.getFullData()
+            print(str(dat['u-out'])+"V, "+str(dat['i-out'])+"A")
+            csv.write(str(dat['u-out']) + ',' + str(dat['i-out'])+"\n")
+
 
 def main():
-    dps = pydps.dps_psu('COM3', 1) # port name, slave address (in decimal)
+    with open('results.csv', 'w') as csv:
+        csv.write("u-out,i-out\n")
+
+    dps.setKeyLock(True)
     print(dps.getModel())
-    print(dps.getFullData())
+
     dps.setVoltage(0)
     dps.setOutput(True)
-    for v in range(0,1400,50):
-        dps.setVoltage(v*0.01)
-        time.sleep(0.25) #wait for stable voltage
-        dat=dps.getFullData()
-        print(str(dat["i-out"]) + 'A ' + str(dat['u-out']) + 'V')
-    dps.setOutput(False)
+    measureSteps(start=0,stop=15,step=0.25)
 
+    dps.setOutput(False)
+    dps.setKeyLock(False)
+
+    print("Finished")
 
 if __name__ == "__main__":
     main()
